27b1e93d5b02f9954d15b08282ea0a2a2843b98f
Sankar491 <sivasankarrcks@gmail.com>
2024-12-24 13:14:39 +0530

    terraform

--- output.tf
+++ output.tf
@@ -18,3 +18,11 @@ output "public_subnets_id" {
   value = aws_subnet.public[*].id
 }
 
+output "private_subnets_id" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnets_id" {
+  value = aws_subnet.database[*].id
+}
+
